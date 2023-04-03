3a098dd4e081c00f80c486a664840d0599aadc7a
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2023-04-03 20:42:52 +0300

    Improved signin and signup

--- client/src/routes/(guest-only)/signin/+page.svelte
+++ client/src/routes/(guest-only)/signin/+page.svelte
@@ -1,12 +1,52 @@
 <script lang="ts">
 	import backendService from '$lib/backend-service';
+    import validationHelper from '$lib/validation-helper';
+    import Button, { Label as ButtonLabel } from '@smui/button';
+    import { Icon } from '@smui/common';
+    import Textfield from '@smui/textfield';
+    import HelperText from '@smui/textfield/helper-text';
+
+	let error = '';
 
 	let email = '';
-	let password = '';
+	let emailError = '';
+	let isEmailInvalid = false;
 
-	async function handleSubmit(e: { preventDefault: () => void; }) {
-		e.preventDefault();
+	let password = '';
+	let passwordError = '';
+	let isPasswordInvalid = false;
+
+	async function handleSubmit() {
+		validateEmail();
+		validatePassword();
+		if (isEmailInvalid || isPasswordInvalid) {
+			return;
+		}
 		let { isOk, message } = await backendService.login(email, password);
+		if (!isOk) {
+			error = message;
+		}
+	}
+
+	function validateEmail() {
+		let error = validationHelper.validateEmail(email);
+		if (error) {
+			isEmailInvalid = true;
+			emailError = error;
+		} else {
+			isEmailInvalid = false;
+			emailError = "";
+		}
+	}
+	function validatePassword() {
+		let error = validationHelper.validatePassword(password);
+		if (error) {
+			isPasswordInvalid = true;
+			passwordError = error;
+		} else {
+			isPasswordInvalid = false;
+			passwordError = "";
+		}
 	}
 </script>
 
@@ -14,11 +54,54 @@
 	<title>Sign in | Background generator</title>
 </svelte:head>
 
-<h1>Login</h1>
-<div>
-	<form on:submit={handleSubmit}>
-		<input type="text" name="email" placeholder="Email" bind:value={email} />
-		<input type="password" name="password" placeholder="Password" bind:value={password} />
-		<button>Login</button>
-	</form>
+<h1>Sign in</h1>
+
+<div id="container">
+	{#if error !== ''}
+	<div id="error">{error}</div>
+	{/if}
+	<Textfield
+		on:blur={validateEmail}
+		bind:value={email}
+		bind:invalid={isEmailInvalid}
+		type="email"
+	>
+		<svelte:fragment slot="label">
+			<Icon
+				class="material-icons"
+				style="font-size: 1em; line-height: normal; vertical-align: top;"
+			>email</Icon> Email
+		</svelte:fragment>
+		<HelperText validationMsg slot="helper">{emailError}</HelperText>
+	</Textfield>
+	<Textfield
+		on:blur={validatePassword}
+		bind:value={password}
+		bind:invalid={isPasswordInvalid}
+		label="Password" type="password"
+	>
+		<HelperText validationMsg slot="helper">{passwordError}</HelperText>
+	</Textfield>
 </div>
+
+<Button on:click={handleSubmit}>
+	<ButtonLabel>Sign in</ButtonLabel>
+</Button>
+<a href="/signup">Don't have an account? Sign up!</a>
+
+<style>
+#error {
+	padding: 5px 10px;
+	background: #fcd0cf;
+	border-radius: 5px;
+	border: #fea9a7 1px solid;
+	width: 100%;
+	color: #b61f1a;
+	box-sizing: border-box;
+}
+#container {
+	display: flex;
+	flex-direction: column;
+	width: 300px;
+}
+</style>
--- client/src/routes/(guest-only)/signup/+page.svelte
+++ client/src/routes/(guest-only)/signup/+page.svelte
@@ -1,12 +1,66 @@
 <script lang="ts">
 	import backendService from '$lib/backend-service';
+    import validationHelper from '$lib/validation-helper';
+    import Button, { Label as ButtonLabel } from '@smui/button';
+    import { Icon } from '@smui/common';
+    import Textfield from '@smui/textfield';
+    import HelperText from '@smui/textfield/helper-text';
+
+	let error = '';
 
 	let email = '';
-	let password = '';
+	let emailError = '';
+	let isEmailInvalid = false;
 
-	async function handleSubmit(e: { preventDefault: () => void; }) {
-		e.preventDefault();
+	let password = '';
+	let passwordError = '';
+	let isPasswordInvalid = false;
+
+	let passwordRepeat = '';
+	let passwordRepeatError = '';
+	let isPasswordRepeatInvalid = false;
+
+	async function handleSubmit() {
+		validateEmail();
+		validatePassword();
+		validatePasswordRepeat();
+		if (isEmailInvalid || isPasswordInvalid || isPasswordRepeatInvalid) {
+			return;
+		}
 		let { isOk, message } = await backendService.register(email, password);
+		if (!isOk) {
+			error = message;
+		}
+	}
+
+	function validateEmail() {
+		let error = validationHelper.validateEmail(email);
+		if (error) {
+			isEmailInvalid = true;
+			emailError = error;
+		} else {
+			isEmailInvalid = false;
+			emailError = "";
+		}
+	}
+	function validatePassword() {
+		let error = validationHelper.validatePassword(password);
+		if (error) {
+			isPasswordInvalid = true;
+			passwordError = error;
+		} else {
+			isPasswordInvalid = false;
+			passwordError = "";
+		}
+	}
+	function validatePasswordRepeat() {
+		if (password !== passwordRepeat) {
+			isPasswordRepeatInvalid = true;
+			passwordRepeatError = "Passwords don't match";
+		} else {
+			isPasswordRepeatInvalid = false;
+			passwordRepeatError = "";
+		}
 	}
 </script>
 
@@ -15,10 +69,61 @@
 </svelte:head>
 
 <h1>Sign up</h1>
-<div>
-	<form on:submit={handleSubmit}>
-		<input type="text" name="email" placeholder="Email" bind:value={email} />
-		<input type="password" name="password" placeholder="Password" bind:value={password} />
-		<button>Sign up</button>
-	</form>
+
+<div id="container">
+	{#if error !== ''}
+	<div id="error">{error}</div>
+	{/if}
+	<Textfield
+		on:blur={validateEmail}
+		bind:value={email}
+		bind:invalid={isEmailInvalid}
+		type="email"
+	>
+		<svelte:fragment slot="label">
+			<Icon
+				class="material-icons"
+				style="font-size: 1em; line-height: normal; vertical-align: top;"
+			>email</Icon> Email
+		</svelte:fragment>
+		<HelperText validationMsg slot="helper">{emailError}</HelperText>
+	</Textfield>
+	<Textfield
+		on:blur={validatePassword}
+		bind:value={password}
+		bind:invalid={isPasswordInvalid}
+		label="Password" type="password"
+	>
+		<HelperText validationMsg slot="helper">{passwordError}</HelperText>
+	</Textfield>
+	<Textfield
+		on:blur={validatePasswordRepeat}
+		bind:value={passwordRepeat}
+		bind:invalid={isPasswordRepeatInvalid}
+		label="Repeat password" type="password"
+	>
+		<HelperText validationMsg slot="helper">{passwordRepeatError}</HelperText>
+	</Textfield>
 </div>
+
+<Button on:click={handleSubmit}>
+	<ButtonLabel>Sign up</ButtonLabel>
+</Button>
+<a href="/signin">Already have an account? Sign in!</a>
+
+<style>
+#error {
+	padding: 5px 10px;
+	background: #fcd0cf;
+	border-radius: 5px;
+	border: #fea9a7 1px solid;
+	width: 100%;
+	color: #b61f1a;
+	box-sizing: border-box;
+}
+#container {
+	display: flex;
+	flex-direction: column;
+	width: 300px;
+}
+</style>
